f30a656e4a83d1b3225b57386cb4641ff0ecd63f
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-02-18 16:11:53 +0100

    init

---
+++ impl/src/main/java/fr/insee/rmes/metadata/api/GeoEndpoints.java
@@ -0,0 +1,29 @@
+package fr.insee.rmes.metadata.api;
+import fr.insee.rmes.metadata.model.DepartementListeDescendantsInner;
+import fr.insee.rmes.metadata.requestprocessor.DescendantsRequestParametizer;
+import fr.insee.rmes.metadata.model.TypeEnumInclusDansDepartement;
+import fr.insee.rmes.metadata.requestprocessor.RequestProcessorBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static fr.insee.rmes.metadata.utils.EndpointsUtils.toResponseEntity;
+
+@Controller
+public class GeoEndpoints implements GeoDepartementApi {
+
+    private final RequestProcessorBuilder requestProcessorBuilder;
+
+    public GeoEndpoints(RequestProcessorBuilder requestProcessorBuilder) {
+        this.requestProcessorBuilder = requestProcessorBuilder;
+    }
+
+    @Override
+    public ResponseEntity<List<DepartementListeDescendantsInner>> getcogdepdesc(String code, LocalDate date, TypeEnumInclusDansDepartement type, String filtreNomDescendant) {
+        return toResponseEntity(requestProcessorBuilder.findDescendants(
+                        new DescendantsRequestParametizer(code, date, type, filtreNomDescendant))
+                .listResult(DepartementListeDescendantsInner.class));
+    }
+}
---
+++ impl/src/main/java/fr/insee/rmes/metadata/requestprocessor/RequestProcessor.java
@@ -0,0 +1,29 @@
+package fr.insee.rmes.metadata.requestprocessor;
+
+import fr.insee.rmes.metadata.queries.QueryBuilder;
+import fr.insee.rmes.metadata.queryexecutor.QueryExecutor;
+import fr.insee.rmes.metadata.unmarshaller.Unmarshaller;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+public record RequestProcessor(QueryBuilder queryBuilder, Map<String, Object> parameters, Path queryFile, QueryExecutor queryExecutor, Unmarshaller unmarshaller) {
+
+    public <E> List<E> listResult(Class<E> clazz) {
+        String typeOrigine;
+        if (clazz.getSimpleName().startsWith("Departement")) {
+             typeOrigine = "Departement" ;
+        } else if (clazz.getSimpleName().startsWith("Commune")) {
+            typeOrigine = "Commune" ;
+        } else {
+            typeOrigine = clazz.getSimpleName();
+        }
+
+        parameters.put("typeOrigine", typeOrigine);
+
+//        parameters.put("typeOrigine", clazz.getSimpleName());
+        var query = queryBuilder.build(parameters, queryFile);
+        return unmarshaller.unmarshalList(queryExecutor.execute(query), clazz);
+    }
+}
